1b4510a8387bb3ed14e04d338c6e1e8d45dbc54b
Marius <realmayus@gmail.com>
2023-09-17 20:40:52 +0200

    linting

--- src/systems/src/io/src/world_loader.rs
+++ src/systems/src/io/src/world_loader.rs
@@ -11,13 +11,13 @@ use lib::scene_serde::SceneSerde;
 // Loads scenes from a scenes.json file
 fn load_scenes(
     path: &str,
-    allocator: &StandardMemoryAllocator,
-    cmd_buf_builder: &mut AutoCommandBufferBuilder<PrimaryAutoCommandBuffer>,
-    default_material: Rc<RefCell<Material>>,
-    tex_i: &mut u32,
-    mat_i: &mut u32,
+    _allocator: &StandardMemoryAllocator,
+    _cmd_buf_builder: &mut AutoCommandBufferBuilder<PrimaryAutoCommandBuffer>,
+    _default_material: Rc<RefCell<Material>>,
+    _tex_i: &mut u32,
+    _mat_i: &mut u32,
 ) {
     let data = fs::read(path).expect("Couldn't read world.json");
-    let scenes: Vec<SceneSerde> =
+    let _scenes: Vec<SceneSerde> =
         serde_json::from_slice(data.as_slice()).expect("Couldn't parse json file");
 }
